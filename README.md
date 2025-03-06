38683dba7c089ca6e990eee6b9cf185f61cb1f22
lucas992x <lucas992x@gmx.com>
2025-03-06 01:16:59 +0100

    Added script to generate background image from pattern

--- README.md
+++ README.md
@@ -3,7 +3,9 @@ Utility code to serve Pokémon Central socials purposes.
 
 ## Scripts
 Each script contains detailed description and list of arguments, here is a recap:
-- `generate-pattern-tile.sh`: generates a tile that can be used to generate a pattern.
+- `generate-background.sh`: generates a background image from a pattern.
+- `generate-pattern-tile.sh`: generates a tile that can be used as pattern.
+- `pcc-stickers.sh`: generates stickers for [Telegram chat](t.me/PCentralChat).
 - `social-img.sh`: adds pattern background and (optionally) text to an image that needs to be posted on socials.
 - `social-img-batch.sh`: invokes `social-img.sh` in batch.
 
---
+++ generate-background.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+help="This script generates an image from a pattern contained in patterns/ directory,
+saving it in backgrounds/ directory. Arguments:
+-p: pattern to use (only part between 'pattern-' and '.png')."
+# get script directory
+script_dir="$(dirname "$(realpath $0)")"
+# parse args
+while getopts "hp:s:" arg; do
+    case $arg in
+    h)
+        echo "$help"
+        exit 0
+        ;;
+    p)
+        pattern="$OPTARG"
+        ;;
+    s)
+        output_size="$OPTARG"
+        ;;
+    *)
+        exit 1
+        ;;
+    esac
+done
+# set default values
+output_size="${output_size:-1080x1920}"
+pattern="${pattern:-yellow-white-50}"
+# set paths of pattern file and output file
+pattern_file="$script_dir/patterns/pattern-$pattern.png"
+output_file="$script_dir/backgrounds/background-$pattern-$output_size.png"
+# generate image
+convert -size "$output_size" tile:"$pattern_file" "$output_file"
+# print final message
+echo "Background image generated from '$pattern_file' and saved as '$output_file'"
